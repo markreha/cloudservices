5d917db7a4919efec45ba1ff4b4de36758985396
markreha <markreha@mac.com>
2017-04-29 09:50:54 -0700

    Finished backend implementation.

--- src/edu/gcu/web/service/RestService.java
+++ src/edu/gcu/web/service/RestService.java
@@ -9,17 +9,23 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import edu.gcu.models.WeatherSensorModel;
-import edu.gcu.models.ResponseModel;
+import edu.gcu.business.WeatherServiceInterface;
+import edu.gcu.model.ResponseDataModel;
+import edu.gcu.model.ResponseModel;
+import edu.gcu.model.WeatherSensorModel;
+
 
 @Path("dht11")
 public class RestService
 {
     Logger logger = LoggerFactory.getLogger(RestService.class);
-
+	static WeatherServiceInterface service;
+	
     /**
      * Test Service API at /test using HTTP GET.
      * 
@@ -55,10 +61,10 @@ public class RestService
     	logger.debug("Model: " + model.toString());
     	
     	// Call Business Service to save the Sensor Data
-    	// TODO: Need to implement the Business Service to save the Sensor Data
+    	boolean OK = service.saveTemperatureSensorData(model);
     	
     	// Return OK Response
-    	ResponseModel response = new ResponseModel(0, "OK");
+    	ResponseModel response = new ResponseModel(OK ? 1 : 0, OK ? "OK" : "Error");
     	return response;
     }
 
@@ -71,30 +77,42 @@ public class RestService
     @GET
     @Path("/get/{device}/{from}/{to}")
     @Produces(MediaType.APPLICATION_JSON)
-    public ResponseModel getTemperatureSensorDataInRange(@PathParam("device") int deviceID, @PathParam("from") String from, @PathParam("to") String to)
+    public ResponseDataModel getTemperatureSensorDataInRange(@PathParam("device") int deviceID, @PathParam("from") String from, @PathParam("to") String to)
     {
        	// Log the API call
     	logger.info("Entering RestService.getTemperatureSensorDataInRange()");
     	logger.debug("Device ID: " + deviceID + " From: " + from + " To: " + to);
     	
-    	// Call Business Service to get the Sensor Data in a Date Range
-    	// TODO: Need to implement the Business Service to get the Sensor Data in a Date Range
-    	SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+    	// Parse parameters and call Business Service to get the Sensor Data in a Date Range
+    	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     	try 
     	{
+        	// Call Business Service to get the Sensor Data in a Date Range
 			Date fromDate = format.parse(from);
 			Date toDate = format.parse(to);
+			List<WeatherSensorModel>data = service.getTemperatureSensorDataInRange(deviceID, fromDate, toDate);
+			
+	    	// Return Response and Data
+	    	int returnError = 0;
+	    	String returnMessage = "";
+	    	if(data.isEmpty())
+	    	{
+	    		returnError = -1;
+	    		returnMessage = "Sensor Data Not Found";
+	    	}
+	    	else
+	    	{
+	    		returnError = 0;
+	    		returnMessage = "OK";
+	    	}
+	    	ResponseDataModel response = new ResponseDataModel(returnError, returnMessage, data);
+	    	return response;
 		} 
     	catch (ParseException e) 
     	{
-	    	ResponseModel response = new ResponseModel(-1, "Bad Parameter Value");
+    		ResponseDataModel response = new ResponseDataModel(-2, "Bad Parameter Value", new ArrayList<WeatherSensorModel>());
 	    	return response;
 		}
-    	
-    	
-    	// Return OK Response
-    	ResponseModel response = new ResponseModel(0, "OK");
-    	return response;
     }
 
     /**
@@ -107,17 +125,43 @@ public class RestService
     @GET
     @Path("/get/{device}/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public ResponseModel getTemperatureSensorData(@PathParam("device") int deviceID, @PathParam("id") int id)
+    public ResponseDataModel getTemperatureSensorData(@PathParam("device") int deviceID, @PathParam("id") int id)
     {
        	// Log the API call
     	logger.info("Entering RestService.getTemperatureSensorData()");
     	logger.debug("Device ID: " + deviceID + " Sensor Data ID: " + id);
     	
     	// Call Business Service to get the Sensor Data for a specified Sensor Data ID
-    	// TODO: Need to implement the Business Service to get the Sensor Data for a specified Sensor Data ID
+    	WeatherSensorModel data = service.getTemperatureSensorData(deviceID, id);
     	
-    	// Return OK Response
-    	ResponseModel response = new ResponseModel(0, "OK");
+    	// Return Response and Data
+    	int returnError = 0;
+    	String returnMessage = "";
+    	List<WeatherSensorModel> returnData = new ArrayList<WeatherSensorModel>();
+    	if(data == null)
+    	{
+    		returnError = -1;
+    		returnMessage = "Sensor Data Not Found";
+    	}
+    	else
+    	{
+    		returnError = 0;
+    		returnMessage = "OK";
+    		returnData.add(data);
+    	}
+    	ResponseDataModel response = new ResponseDataModel(returnError, returnMessage, returnData);
     	return response;
+    }
+
+    // ***** Dependencies and Helper Functions *****
+    
+    /**
+     * IoC helper function.
+     * 
+     * @param dao WeatherServiceInterface to inject into this service implementation.
+     */
+    public void setService(WeatherServiceInterface service)
+    {
+    	RestService.service = service;
     }
 }
